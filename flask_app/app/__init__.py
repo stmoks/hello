4f51ea935696514891f5288b9f8e1fef37157c65
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-08-12 20:37:02 +0200

    initialise db

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,24 +1,28 @@
 import os
 from flask import Flask
 
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app(test_config=None):
     #create and configure the app
     application = Flask(__name__)
-    # application.config.from_prefixed_env()
-    # print(application.config.get('DATABASE'))
-    # print(os.getenv('ENVIRONMENT'))
+    application.config.from_prefixed_env()
+    print(f'Database: {application.config.get('DATABASE')}')
+    print(f'Environment: {os.getenv('ENVIRONMENT')}')
 
+    # register database
+    import app.database as database
+    database.init_app(application)
 
-    # import pages blueprint
+    # register pages blueprint
     from app.views import pages
     application.register_blueprint(pages.bp)
 
     # register posts blueprint
     from app.views import posts
     application.register_blueprint(posts.bp)
+
 
     return application
 

17144a9831c6dcfd1eebdd613c1ce67d44b758ba
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-08-16 06:42:32 +0200

    start adding postgres config, create postgres db and schema'

--- flask/app/database.py
+++ flask/app/database.py
@@ -1,6 +1,9 @@
 import sqlite3
 import click
 from flask import current_app, g
+from sqlalchemy import create_engine
+import psycopg2
+
 
 def init_app(app):
     app.cli.add_command(init_db_command)
@@ -11,17 +14,19 @@ def init_db_command():
     db = get_db()
 
     with current_app.open_resource("schema.sql") as f:
-        db.executescript(f.read().decode("utf-8"))
+        # db.executescript(f.read().decode("utf-8"))
+        db.execute(f.read().decode("utf-8"))
 
     click.echo("You successfully initialized the database!")
 
 def get_db():
     if "db" not in g:
-        g.db = sqlite3.connect(
-            current_app.config["DATABASE"],
-            detect_types=sqlite3.PARSE_DECLTYPES,
-        )
-        g.db.row_factory = sqlite3.Row
+        # g.db = sqlite3.connect(
+        #     current_app.config["DATABASE"],
+        #     detect_types=sqlite3.PARSE_DECLTYPES,
+        # )
+        # g.db.row_factory = sqlite3.Row
+        g.db = create_engine(f'postgresql+psycopg2://postgres:postgres@localhost:5432/users')
 
     return g.db
 
@@ -30,4 +35,5 @@ def close_db(e=None):
     db = g.pop("db", None)
 
     if db is not None:
-        db.close()
+        # db.close()
+        db.dispose()

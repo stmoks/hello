1e070d344c2cc49ba55c12545c54917212caabc9
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-08-18 21:41:05 +0200

    add folium iframe

--- flask/app/views/pages.py
+++ flask/app/views/pages.py
@@ -1,11 +1,13 @@
-from flask import Blueprint,render_template
+import folium
+from flask import Blueprint,render_template,render_template_string
 
 bp = Blueprint('pages',__name__)
 
 @bp.route('/')
 def home():
-    return render_template('pages/home.html')
+    m = folium.Map()
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+    return render_template('pages/home.html',iframe=iframe)
 
-@bp.route('/about')
-def about():
-    return render_template('pages/about.html')

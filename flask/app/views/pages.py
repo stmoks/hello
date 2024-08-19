8640f9baf2eb8e9250979bedfd8c302da2b93034
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-08-19 19:59:04 +0200

    add fullscreen folium

--- flask/app/views/pages.py
+++ flask/app/views/pages.py
@@ -5,9 +5,5 @@ bp = Blueprint('pages',__name__)
 
 @bp.route('/')
 def home():
-    m = folium.Map()
-    m.get_root().width = "800px"
-    m.get_root().height = "600px"
-    iframe = m.get_root()._repr_html_()
-    return render_template('pages/home.html',iframe=iframe)
+    return render_template('pages/home.html')
 
--- flask/app/views/posts.py
+++ flask/app/views/posts.py
@@ -1,5 +1,9 @@
 from sqlalchemy import text
 from flask import Blueprint,redirect,render_template,request,url_for
+import folium
+import streamlit
+from streamlit_folium import st_folium
+
 
 bp = Blueprint('posts',__name__)
 
@@ -28,3 +32,18 @@ def posts():
         text('SELECT author, message, created FROM users.post ORDER BY created DESC')
     ).fetchall()
     return render_template('posts/posts.html',posts=posts)
+
+
+@bp.route('/map',methods=['GET'])
+def map():
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    folium.Marker(
+        [39.949610, -75.150282], popup='Liberty Bell', tooltip='Liberty Bell'
+    ).add_to(m)
+
+    # # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725)
+       
+    return m.get_root().render()
+    
+    

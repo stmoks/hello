c8922978c187e0fc8e6f91cd1b4ad174c460507e
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-09-04 20:01:25 +0200

    clean remove page, reorganise css for maps

--- flask/app/views/posts.py
+++ flask/app/views/posts.py
@@ -1,29 +1,11 @@
 from sqlalchemy import text
 from flask import Blueprint,redirect,render_template,request,url_for
 import folium
-import streamlit
-from streamlit_folium import st_folium
-
 
 bp = Blueprint('posts',__name__)
 
 from app.database import get_db
 
-@bp.route('/explore',methods=['GET','POST'])
-def search():
-    if request.method == 'POST':
-        author = request.form['author'] or 'Anonymous'
-        message = request.form['message']
-
-        if message:
-            db = get_db()
-            db.execute(
-                text("INSERT INTO users.post (author, message) VALUES (:a, :m)"),
-                {'a': author, 'm':message}
-            )
-            db.commit()
-            return redirect(url_for("posts.posts"))
-    return render_template('posts/explore.html')
 
 @bp.route('/posts',methods=['GET'])
 def posts():
